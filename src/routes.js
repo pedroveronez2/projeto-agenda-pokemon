bff9f5ec1de8370ea0d40966feea4b1aa5abeb58
Pedro Augusto <>
2023-07-21 14:24:09 -0300

    adicionando arquivos atualizados

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const homeController = require('./controllers/homeController')
-const route = express.Router()
-
-route.get('/', homeController.paginaInicial)
-
-module.exports = route
